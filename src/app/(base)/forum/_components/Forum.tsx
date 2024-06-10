c9b51cecdf939ba076e0069c5c69c99a21b9514a
ginlord <nicandro.ronnie@gmail.com>
2024-06-11 02:15:15 +0800

    added popular sortng in forum

--- src/app/(base)/forum/_components/Forum.tsx
+++ src/app/(base)/forum/_components/Forum.tsx
@@ -1,23 +1,25 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import ForumContainer from "@/components/forum/ForumContainer";
 import MaxWidthWrapper from "@/components/layout/MaxWidthWrapper";
-import { Forum,  } from "@prisma/client";
+import { Forum } from "@prisma/client";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import ForumSkeleton from "@/components/forum/ForumSkeleton";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 const Forums = () => {
-  const searchParams = useSearchParams()
-  const {replace,refresh} = useRouter()
-  const pathname = usePathname()
-  const [sortAs, setSortAs] = useState<"oldest" | "newest">(searchParams.get("sort") as "newest" | "oldest")
+  const searchParams = useSearchParams();
+  const { replace} = useRouter();
+  const pathname = usePathname();
+  const [sortAs, setSortAs] = useState<"oldest" | "newest" | "popular">(
+    searchParams.get("sort") as "newest" | "oldest" | "popular"
+  );
   const getForums = async ({ cursor }: { cursor: string }) => {
     const params = new URLSearchParams({
       cursor: cursor,
-      sort:sortAs,
+      sort: sortAs,
     });
     const forum = await fetch(`api/forum?${params}`);
     const response = await forum.json();
@@ -26,16 +28,20 @@ const Forums = () => {
   };
 
   const userLikeForums = async () => {
-    const response = await fetch("api/forum/like")
-    const data:any = await response.json()
-    return data.userLikes
-  }
- 
-  const {data:userLikes, error,refetch:refetchUserLikes} = useQuery({
-    queryKey:["user-likes"],
-    queryFn:userLikeForums,
-  })
-    const {
+    const response = await fetch("api/forum/like");
+    const data: any = await response.json();
+    return data.userLikes;
+  };
+
+  const {
+    data: userLikes,
+    error,
+    refetch: refetchUserLikes,
+  } = useQuery({
+    queryKey: ["user-likes"],
+    queryFn: userLikeForums,
+  });
+  const {
     data,
     isSuccess,
     isPending,
@@ -55,43 +61,53 @@ const Forums = () => {
         : undefined;
     },
   });
-  
+
   const { ref, inView } = useInView();
-  useEffect(()=>{
-    if(!sortAs){
-      setSortAs(searchParams.get("sort") as "newest" || "oldest")
+  useEffect(() => {
+    if (!sortAs) {
+      setSortAs((searchParams.get("sort") as "newest" || "oldest" || "popular"));
     }
     replace(`${pathname}?sort=${sortAs ?? "newest"}`, {
-      scroll:false
+      scroll: false,
     });
-    refetch()
-  refetchUserLikes;
-  }, [sortAs,setSortAs,pathname,replace, refetchUserLikes])
+    refetch();
+    refetchUserLikes();
+  }, [sortAs, setSortAs, pathname, replace, refetchUserLikes]);
   useEffect(() => {
     // if the last element is in view and there is a next page, fetch the next page
     if (inView && hasNextPage && !isRefetching) {
       fetchNextPage();
     }
-  }, [fetchNextPage, hasNextPage, inView,isRefetching]);
+  }, [fetchNextPage, hasNextPage, inView, isRefetching]);
   return (
     <div>
-      
-      <MaxWidthWrapper className="flex justify-center  "> {/*border-l-1 border-gray-400*/}
+      <MaxWidthWrapper className="flex justify-center  ">
+        {" "}
+        {/*border-l-1 border-gray-400*/}
         <div className="flex flex-col">
-        {/* TODO: MOVE THIS BUTTON TO THE NAVBAR OR IN SIDEBAR*/}
+          {/* TODO: MOVE THIS BUTTON TO THE NAVBAR OR IN SIDEBAR*/}
           <Link href="/new" className="flex items-center justify-center mt-5">
-          <Button >Create Post</Button> 
+            <Button>Create Post</Button>
           </Link>
           <div className="flex items-end w-full justify-end ">
-            <Button variant="link"
-            onClick={()=>setSortAs((prev) => prev ="oldest")}
-            className={`${sortAs === "oldest"  &&   'underline'}`}
+            <Button
+              variant="link"
+              onClick={() => setSortAs((prev) => (prev = "popular"))}
+              className={`${sortAs === "popular" && "underline"}`}
+            >
+              Popular
+            </Button>
+            <Button
+              variant="link"
+              onClick={() => setSortAs((prev) => (prev = "oldest"))}
+              className={`${sortAs === "oldest" && "underline"}`}
             >
               Oldest
             </Button>
-            <Button variant="link"
-             onClick={()=>setSortAs((prev) => prev ="newest")}
-             className={`${sortAs === "newest"  &&   'underline'}`}
+            <Button
+              variant="link"
+              onClick={() => setSortAs((prev) => (prev = "newest"))}
+              className={`${sortAs === "newest" && "underline"}`}
             >
               Newest
             </Button>
@@ -100,27 +116,36 @@ const Forums = () => {
             data.pages.map(
               (page) =>
                 page.data &&
-                page.data.map((forum: Forum & {_count:{forumLikes:number,comments:number}}, index:number) => (
-                  <div key={index} ref={ref}>
-                    <ForumContainer {...forum} userLikes={userLikes}  />
-                  </div>
-                ))
+                page.data.map(
+                  (
+                    forum: Forum & {
+                      _count: { forumLikes: number; comments: number };
+                    },
+                    index: number
+                  ) => (
+                    <div key={index} ref={ref}>
+                      <ForumContainer {...forum} userLikes={userLikes} />
+                    </div>
+                  )
+                )
             )
           ) : (
             <ForumSkeleton />
           )}
           {isFetchingNextPage && <ForumSkeleton />}
           {!hasNextPage && !isPending && !isLoading && (
-             <div className="relative mt-10 mx-[130px]">
-             <div className="flex items-center inset-0 absolute ">
-               <span className=" border-t w-full border-1 border-gray-500"> </span>
-             </div>
-             <div className="relative flex justify-center items-center  font-bold">
-               <span className="text-muted-foreground text-md px-3 bg-[#eff1f5]">
-                 No more posts
-               </span>
-             </div>
-           </div>
+            <div className="relative mt-10 mx-[130px]">
+              <div className="flex items-center inset-0 absolute ">
+                <span className=" border-t w-full border-1 border-gray-500">
+                  {" "}
+                </span>
+              </div>
+              <div className="relative flex justify-center items-center  font-bold">
+                <span className="text-muted-foreground text-md px-3 bg-[#eff1f5]">
+                  No more posts
+                </span>
+              </div>
+            </div>
           )}
         </div>
       </MaxWidthWrapper>
--- src/app/api/forum/route.ts
+++ src/app/api/forum/route.ts
@@ -6,11 +6,10 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/authOptions";
 import { revalidatePath } from "next/cache";
 export async function GET(req: NextRequest): Promise<any> {
-
   try {
     const url = new URL(req.url);
     const cursor = url.searchParams.get("cursor");
-    const sort =  url.searchParams.get("sort")
+    const sort = url.searchParams.get("sort");
     const forums = await prisma.forum.findMany({
       ...(cursor && {
         skip: 1,
@@ -19,17 +18,27 @@ export async function GET(req: NextRequest): Promise<any> {
         },
         take: 1,
       }),
-      include:{
-        _count:{
-    select:{
-      forumLikes:true,
-      comments:true,
-    }
-        }
+      include: {
+        _count: {
+          select: {
+            forumLikes: true,
+            comments: true,
+          },
+        },
       },
-      orderBy:{
-        createdAt: sort === "newest" ? "desc" : sort === "oldest" ? "asc" : "desc", // SORTING POST  I SET THE NEWEST SORT FOR THE DEFAULT HERE
-      }
+      orderBy: {
+        /// SORT THE FORUM, DEFAULT IS THE NEWEST SORT
+        ...(sort === "popular"
+          ? {
+              forumLikes: {
+                _count: "desc",
+              },
+            }
+          : {
+              createdAt:
+                sort === "newest" ? "desc" : sort === "oldest" ? "asc" : "desc",
+            }),
+      },
     });
     if (forums.length === 0) {
       return NextResponse.json(
@@ -53,8 +62,8 @@ export async function GET(req: NextRequest): Promise<any> {
       take: 1,
       skip: 1,
     });
-    const data =  {
-      data:  forums,
+    const data = {
+      data: forums,
       metaData: {
         lastCursor: newCursor !== undefined ? newCursor : null,
         hasNextPage: nextForum.length > 0,
@@ -78,45 +87,48 @@ export async function GET(req: NextRequest): Promise<any> {
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
-  let imageUpload:any
+  let imageUpload: any;
   const body = await req.formData();
   const image = body.get("image") as unknown as File;
-  if(image){
-     imageUpload = await uploadCloudinary(image, "algorithmic-oddysey");
+  if (image) {
+    imageUpload = await uploadCloudinary(image, "algorithmic-oddysey");
   }
   const captions = JSON.parse(body.get("caption") as string);
   const title = JSON.parse(body.get("title") as string);
   /// FOR TITLE ID RANDOM STRING
   function generateRandomString(length = 8) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
     for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
     }
     return result;
   }
-try {
-  await prisma.forum.create({
-    data: {
-      caption: captions ?? null,
-      title: title,
-      titleId:generateRandomString(),
-      forumImage: imageUpload?.secure_url ?? null,
-      user: {
-        connect: {
-          id: session?.user.id,
+  try {
+    await prisma.forum.create({
+      data: {
+        caption: captions ?? null,
+        title: title,
+        titleId: generateRandomString(),
+        forumImage: imageUpload?.secure_url ?? null,
+        user: {
+          connect: {
+            id: session?.user.id,
+          },
         },
       },
-    },
-    select: {
-      caption: true,
-      forumImage: true,
-      title: true,
-    },
-  });
-  revalidatePath("/forum", "page")
-  return NextResponse.json({message:"SUCCESFULLY ADDED"}, {status:200})
-} catch (error) {
-  return NextResponse.json({message:error}, {status:500})
-}
+      select: {
+        caption: true,
+        forumImage: true,
+        title: true,
+      },
+    });
+    revalidatePath("/forum", "page");
+    return NextResponse.json({ message: "SUCCESFULLY ADDED" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
