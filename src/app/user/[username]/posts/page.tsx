48e671b6c55c7168ba1d67932492c5f2dc812816
ginlord <nicandro.ronnie@gmail.com>
2024-07-30 01:16:10 +0800

    fix the user likes in the profile view

--- src/app/user/[username]/posts/page.tsx
+++ src/app/user/[username]/posts/page.tsx
@@ -1,15 +1,14 @@
-import ForumContainer from "@/components/forum/ForumContainer";
 import prisma from "@/db";
 import { authOptions } from "@/utils/authOptions";
-import { Forum, ForumLike } from "@prisma/client";
+import { ForumLike } from "@prisma/client";
 import { Loader2 } from "lucide-react";
 import { getServerSession } from "next-auth";
 import React, { Fragment, Suspense } from "react";
 import UserProfilePosts from "./_components/UserProfilePosts";
-import { ForumWithCount, UserProfilePostsProps } from "@/types/types";
 const UserPostsPage = async ({ params }: { params: { username: string } }) => {
   const session = await getServerSession(authOptions)
   const { username } = params;
+  let userLikes:ForumLike[] = [];
   const userPosts = await prisma.user.findUnique({
     where: {
       username: decodeURIComponent(username),
@@ -31,11 +30,14 @@ const UserPostsPage = async ({ params }: { params: { username: string } }) => {
   });
     if (!userPosts) return null;
   const { forums } = userPosts;
-  const userLikes: ForumLike[] = await prisma.forumLike.findMany({
-    where: {
-        userId:session?.user.id
-    },
-  });
+
+  if(session?.user.id){
+     userLikes = await prisma.forumLike.findMany({
+      where: {
+          userId:session.user.id
+      },
+    }) ?? [];
+  }
 
   return (
     <Suspense fallback={<Loader2 className="h-16 w-16 animate-spin"/>}> 
--- src/components/forum/ForumContainer.tsx
+++ src/components/forum/ForumContainer.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import parse from "html-react-parser";
 import { JSONContent } from "@tiptap/react";
-import GenerateHtmlContent from "./generateHtmlContent";
+import GenerateHtmlContent from "./GenerateHtmlContent";
 const ForumContainer = ({
   id,
   authorUsername,
@@ -22,45 +22,45 @@ const ForumContainer = ({
   userImage,
   followBtnComponent,
   userId,
-content,
+  content,
 }: Forum & {
-  _count:{
-    forumLikes:number;
-    comments?:number;
-  },
-  userLikes?:ForumLike[],
-  className?:string,
-  followBtnComponent?:React.JSX.Element
+  _count: {
+    forumLikes: number;
+    comments?: number;
+  };
+  userLikes?: ForumLike[];
+  className?: string;
+  followBtnComponent?: React.JSX.Element;
 }) => {
   const timeDiffCalc = useMemo(() => {
     return timeDiff(createdAt);
   }, [createdAt]);
-let tempContent;
-  if(content){
-    tempContent = GenerateHtmlContent(content as JSONContent)
+  let tempContent;
+  if (content) {
+    tempContent = GenerateHtmlContent(content as JSONContent);
   }
-  console.log(tempContent, "CONTET")
   return (
-    <div className={cn("relative pt-20 lg:mx-[300px] md:mx-[100px] mx-0 max-w-xl overflow-hidden w-full",className)}>
+    <div
+      className={cn(
+        "relative pt-20 lg:mx-[300px] md:mx-[100px] mx-0 max-w-xl overflow-hidden w-full",
+        className
+      )}
+    >
       <div className="h-fit w-full pb-[10px]  border-b-1 border-muted-foreground">
         <div className="flex flex-col items-start w-full">
-          <div className="flex flex-row gap-2 justify-between text-xs cursor-pointer w-full "
-         
-          > 
-          {/**TODO: ADD USER IMAGE IN FORUM SCHEMA */}
-            <Link href={`/user/${authorUsername}`} className="flex gap-2"> 
-            <Avatar
-              showFallback
-              src={userImage}
-              size="md"
-            />
-            <div className="flex flex-col">
-              <p className=" tracking-wide font-semibold text-md">{authorUsername}</p>
-              {/* <strong className='pl-1'>.</strong> */}
-              <p className="text-gray-500 ">{timeDiffCalc}</p>
-            </div>
+          <div className="flex flex-row gap-2 justify-between text-xs cursor-pointer w-full ">
+            {/**TODO: ADD USER IMAGE IN FORUM SCHEMA */}
+            <Link href={`/user/${authorUsername}`} className="flex gap-2">
+              <Avatar showFallback src={userImage} size="md" />
+              <div className="flex flex-col">
+                <p className=" tracking-wide font-semibold text-md">
+                  {authorUsername}
+                </p>
+                {/* <strong className='pl-1'>.</strong> */}
+                <p className="text-gray-500 ">{timeDiffCalc}</p>
+              </div>
             </Link>
-          {/* TODO: FETCH IF THE USER IS ALREADY FOLLOWING THE OTHER USER AND PASS IT ON AS PROPS HERE CHECH FORUM BTN USERLIKES PROPS FOR REFERENCE */}
+            {/* TODO: FETCH IF THE USER IS ALREADY FOLLOWING THE OTHER USER AND PASS IT ON AS PROPS HERE CHECH FORUM BTN USERLIKES PROPS FOR REFERENCE */}
             <div className="flex justify-start items-start">
               {followBtnComponent}
             </div>
@@ -70,28 +70,34 @@ let tempContent;
           </div>
           {/* <div className="text-sm text-black dark:text-white tracking-wide">{caption}</div> */}
           {tempContent && (
-            <div className="text-sm text-black dark:text-white tracking-wide">{parse(`${tempContent}`)}</div>
+            <div className="text-sm text-black dark:text-white tracking-wide">
+              {parse(`${tempContent}`)}
+            </div>
           )}
           {forumImage && (
-        <div className="relative w-full max-w-md md:max-w-xl overflow-hidden"
-        >
-          <Link href={`/forum/${authorUsername}/comments/${titleId}/${title}`}> 
-        <Image
-          src={forumImage}
-          alt="Animated GIF"
-          className={cn("rounded-md mt-2 w-full h-auto")}/// adjust the photo size here // TODO : FIX THIS DYNAMIC CLASSNAME
-          width={320}
-          height={400}
-          unoptimized={true}
-          loading="lazy"
-        />
-        </Link>
-      </div>
+            <div className="relative w-full max-w-md md:max-w-xl overflow-hidden">
+              <Link
+                href={`/forum/${authorUsername}/comments/${titleId}/${title}`}
+              >
+                <Image
+                  src={forumImage}
+                  alt="Animated GIF"
+                  className={cn("rounded-md mt-2 w-full h-auto")} /// adjust the photo size here // TODO : FIX THIS DYNAMIC CLASSNAME
+                  width={320}
+                  height={400}
+                  unoptimized={true}
+                  loading="lazy"
+                />
+              </Link>
+            </div>
           )}
-          <ForumButtons likes={_count.forumLikes} forumId={id} userLikes={userLikes} route={`/forum/${authorUsername}/comments/${titleId}/${title}`}
+          <ForumButtons
+            likes={_count.forumLikes}
+            forumId={id}
+            userLikes={userLikes}
+            route={`/forum/${authorUsername}/comments/${titleId}/${title}`}
             comments={_count.comments as number}
             postOwner={userId}
-
           />
         </div>
       </div>
