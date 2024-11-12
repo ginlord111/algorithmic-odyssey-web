ec6d19a3f2de715191e89b2ba0f718a9c7c78a97
ginlord <nicandro.ronnie@gmail.com>
2024-11-12 18:31:52 +0800

    added tmp folder

--- src/app/api/classroom/classwork/route.ts
+++ src/app/api/classroom/classwork/route.ts
@@ -8,7 +8,7 @@ import generateRandomString from "@/helper/generateRandomString";
 /// function for temporary saving the file in the local folder
 async function saveFile(file: File, filename: string) {
   const buffer = Buffer.from(await file.arrayBuffer());
-  const filePath = path.join(process.cwd(), "uploads", filename);
+  const filePath = path.join(process.cwd(), "tmp", filename);
   //@ts-ignore
   await fs.writeFile(filePath, buffer);
   return filePath;
