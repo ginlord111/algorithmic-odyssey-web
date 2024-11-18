171a4ad81dc0420296cb00da0ffa99b15a14290d
Ronnie Nicandro <91516540+ginlord111@users.noreply.github.com>
2024-11-19 01:22:43 +0800

    api/lesson

---
+++ src/app/api/lesson/route.ts
@@ -0,0 +1,37 @@
+import prisma from "@/db";
+import { LessonProgressProps } from "@/types/types";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const { student_id, key, value } = await req.json();
+  try {
+    const data = await prisma.user.findUnique({
+      where: {
+        id: student_id,
+      },
+      select: {
+        lessonProgress: true,
+      },
+    });
+
+    if (!data) {
+      NextResponse.json({ message: "Student not found" }, { status: 404 });
+    }
+   const lessonProgress = data?.lessonProgress as unknown as LessonProgressProps
+   lessonProgress.progress[key] = value;
+   console.log(lessonProgress.progress, "PLSSSS")
+   await prisma.user.update({
+    where:{
+        id:student_id,
+    },
+      data:{
+        lessonProgress:lessonProgress as any,
+      }
+    }
+   );
+    return NextResponse.json({message:"Success"}, {status:200}) 
+  } catch (error) {
+    console.log(error);
+   return NextResponse.json({ error }, { status: 500 });
+  }
+}
