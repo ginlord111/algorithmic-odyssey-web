448ec647aa49fcaab3f5ef8d291fdeb2c5f37096
Ronnie Nicandro <91516540+ginlord111@users.noreply.github.com>
2024-11-19 02:09:05 +0800

    remove www in cors

--- src/app/api/lesson/route.ts
+++ src/app/api/lesson/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/db";
 import { LessonProgressProps } from "@/types/types";
 import { NextRequest, NextResponse } from "next/server";
 const corsHeaders = {
-    "Access-Control-Allow-Origin": "https://algo-thesis.onrender.com", // Replace with a specific origin if needed
+    "Access-Control-Allow-Origin": "algo-thesis.onrender.com", // Replace with a specific origin if needed
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
   };
