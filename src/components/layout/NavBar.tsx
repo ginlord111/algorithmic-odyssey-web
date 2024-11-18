648736cdc3ef33e42a7d775a402285f802493f63
Ronnie Nicandro <91516540+ginlord111@users.noreply.github.com>
2024-11-18 11:24:21 +0800

    fix conflict to merge main

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -63,6 +63,7 @@ export default function NavBar() {
             headers: {
               "Content-Type": "application/json",
             },
+            credentials:"include",
             body: JSON.stringify({ user, session }),
           }
         );
