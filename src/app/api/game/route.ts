b30425f22262f3b7f5991168e4622af2196e4d3e
Ronnie Nicandro <91516540+ginlord111@users.noreply.github.com>
2024-11-25 11:09:34 +0800

    fix leaderboard

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/db";
+import { user } from "@nextui-org/react";
 import { User } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
@@ -23,20 +24,26 @@ try {
 
 
 export async function GET(){
-  const allUser:User[] = await prisma.user.findMany({
-    orderBy:{
-      score:{
-        sort:"asc"
-      }
-    }
-  })
-const kekw = allUser[0].gameState
+  const allUser:User[] = (await prisma.user.findMany({}))
+  
+const sortUser = allUser.sort((a:any,b:any) => {if(a.gameState.levels[0].isCompleted && b.gameState.levels[0].isCompleted){
+return b.gameState.levels[0].points - a.gameState.levels[0].points;
+
+}
+if(a.gameState.levels[0].isCompleted === b.gameState.levels[0].isCompleted){
+  return 0;
+}
+return a.gameState.levels[0].isCompleted ? -1 : 1;
+})
+
+console.log(sortUser, "SORTED USER")  
+
   const headers = new Headers();
   headers.set('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
   headers.set('Access-Control-Allow-Headers', 'Content-Type');
 
-return NextResponse.json({users:allUser}, {status:200, headers})
+return NextResponse.json({users:sortUser}, {status:200, headers})
 }
 
 
